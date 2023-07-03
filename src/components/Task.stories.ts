99ee174fbf0549bcb5f5c5ae0cc4951ec8c21f26
Elena Grednikova <vesnushkalena@gmail.com>
2023-07-03 18:00:14 +0200

    Long text story

--- src/components/Task.stories.ts
+++ src/components/Task.stories.ts
@@ -42,7 +42,7 @@ export const Archived = {
 };
 
 
-const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not! Some more words just to make second commit to the repo`;
 
 export const LongTitle = {
   args: {
